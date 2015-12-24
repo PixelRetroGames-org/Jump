09df58ac3eb454f5247e85c13a621c25749e8cec
PRG <pixelretrogames@github.com>
2015-12-24 16:50:51 +0200

    update1

--- source.cpp
+++ source.cpp
@@ -5,11 +5,14 @@
 #include<cstdio>
 #include<string>
 #include<cstring>
+#include<stdlib.h>
 #include<algorithm>
 #define MAX_LIN 600
 #define MAX_COL 800
-#define LEVELL 10
-#define LEVELC 20
+#define LEVELL 100
+#define LEVELC 100
+#define GRIDL 40
+#define GRIDC 800
 
 void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination )
 {
@@ -47,6 +50,22 @@ SDL_Surface *make_it_transparent( std::string filename )
  return optimizedImage;
 }
 
+void int_to_string(int nr,char *v)
+{
+ int cnr=nr,nr_digits=0;
+ while(cnr!=0)
+       {
+        cnr/=10;
+        nr_digits++;
+       }
+ v[nr_digits]=0;
+ while(nr!=0)
+       {
+        *(v+(--nr_digits))=nr%10+'0';
+        nr/=10;
+       }
+}
+
 SDL_Surface *screen;
 
 struct coordinates
@@ -62,15 +81,26 @@ struct direction
 
 struct player
 {
- direction orizontal,vertical;
+ direction horizontal,vertical;
  int lives,points;
  coordinates level_position,screen_position;
  std::string image_name;
  SDL_Surface *image;
+ void generate_screen_position()
+ {
+  screen_position.x=(level_position.x-1)*40;
+  screen_position.y=(level_position.y-1)*40;
+ }
+ void load_initial_image()
+ {
+  image=make_it_transparent(image_name);
+ }
  bool load_image()
  {
   std::string filename=image_name;
-  filename=(orizontal.val>=0?"R":"L")+filename;
+  if(horizontal.val==0)
+     return false;
+  filename=(horizontal.val>0?"R":"L")+filename;
   image=make_it_transparent(filename);
   if(image!=NULL)
      return true;
@@ -78,7 +108,16 @@ struct player
  }
  void print_image()
  {
-  apply_surface(screen_position.x,screen_position.y,image,screen);
+  apply_surface(screen_position.y,screen_position.x+GRIDL,image,screen);
+ }
+ void erase_image()
+ {
+  SDL_Surface *clear=SDL_LoadBMP("clear.bmp");
+  apply_surface(screen_position.y,screen_position.x+GRIDL,clear,screen);
+ }
+ void jump()
+ {
+  vertical.val=-2;
  }
 };
 
@@ -87,9 +126,14 @@ player timy;
 struct texture
 {
  int code;
+ bool solid;
  void read(FILE *in)
  {
   fscanf(in,"%d ",&code);
+  if(code==1)
+     solid=true;
+  else
+     solid=false;
  }
  void print_image(int x,int y)
  {
@@ -142,22 +186,182 @@ struct level
        for(int j=position.y;j<=C;j++)
            {
             if(i!=timy.level_position.x || j!=timy.level_position.y)
-               MAP[i][j].print_image((j-1)*40,(i-1)*40);
+               MAP[i][j].print_image((j-1)*40,(i-1)*40+GRIDL);
            }
       }
  }
 };
 
+struct Timer
+{
+ //The clock time when the timer started
+ int startTicks;
+ //The ticks stored when the timer was paused
+ int pausedTicks;
+ //The timer status
+ bool paused;
+ bool started;
+ Timer()
+ {
+  //Initialize the variables
+  startTicks = 0;
+  pausedTicks = 0;
+  paused = false;
+  started = false;
+ }
+ void start()
+ {
+  //Start the timer
+  started = true;
+  //Unpause the timer
+  paused = false;
+  //Get the current clock time
+  startTicks = SDL_GetTicks();
+ }
+ void stop()
+ {
+  //Stop the timer
+  started = false;
+  //Unpause the timer
+  paused = false;
+ }
+ void pause()
+ {
+  //If the timer is running and isn't already paused
+  if((started==true) && (paused==false))
+     {
+      //Pause the timer
+      paused = true;
+      //Calculate the paused ticks
+      pausedTicks = SDL_GetTicks() - startTicks;
+     }
+ }
+ void unpause()
+ {
+  //If the timer is paused
+  if(paused==true)
+     {
+      //Unpause the timer
+      paused=false;
+      //Reset the starting ticks
+      startTicks = SDL_GetTicks() - pausedTicks;
+      //Reset the paused ticks
+      pausedTicks = 0;
+     }
+ }
+ int get_ticks()
+ {
+  //If the timer is running
+  if(started==true)
+     {
+      //If the timer is paused
+      if(paused==true)
+         {
+          //Return the number of ticks when the timer was paused
+          return pausedTicks;
+         }
+      else
+         {
+          //Return the current time minus the start time
+          return SDL_GetTicks() - startTicks;
+         }
+     }
+  //If the timer isn't running
+  return 0;
+ }
+ bool is_started()
+ {
+  return started;
+ }
+ bool is_paused()
+ {
+  return paused;
+ }
+};
+
+//Returns if the player is/isn't on solid texture
+bool update_position(level A,player &P)
+{
+ bool pp=false;
+ for(int i=P.level_position.x-1;i>=P.level_position.x+P.vertical.val && !pp;i--)
+     if(A.MAP[i][P.level_position.y].solid)
+        {
+         P.level_position.x=i+1;
+         pp=true;
+        }
+ if(!pp)
+    P.level_position.x+=P.vertical.val;
+ P.level_position.y+=P.horizontal.val;
+ P.level_position.x=std::min(A.L,P.level_position.x);
+ P.level_position.y=std::min(A.C,P.level_position.y);
+ P.level_position.x=std::max(1,P.level_position.x);
+ P.level_position.y=std::max(1,P.level_position.y);
+ if(A.MAP[P.level_position.x][P.level_position.y].solid)
+    {
+     P.level_position.x-=P.vertical.val;
+     P.level_position.y-=P.horizontal.val;
+    }
+ if(P.vertical.val==0)
+    {
+     if(!A.MAP[P.level_position.x+1][P.level_position.y].solid)
+        {
+         P.level_position.x++;
+         P.vertical.val=P.horizontal.val=0;
+         return true;
+        }
+    }
+ P.vertical.val=P.horizontal.val=0;
+ return false;
+}
+
+void print_lives()
+{
+ TTF_Font *font=TTF_OpenFont("font.ttf",30);
+ SDL_Color color={255,46,28};
+ SDL_Surface *message=TTF_RenderText_Solid(font,"LIVES:",color);
+ apply_surface(400,4,message,screen);
+ int POZ=500;
+ SDL_Surface *heart=make_it_transparent("heart.bmp");
+ for(int i=1;i<=timy.lives;i++)
+     {
+      apply_surface(POZ+(i-1)*40,0,heart,screen);
+     }
+}
+
+void print_points()
+{
+ int POZ=120;
+ TTF_Font *font=TTF_OpenFont("font.ttf",30);
+ char nr[90];
+ int_to_string(timy.points,nr);
+ SDL_Color color={255,216,0};
+ SDL_Surface *message=TTF_RenderText_Solid(font,nr,color);
+ apply_surface(POZ,4,message,screen);
+ color={255,216,0};
+ message=TTF_RenderText_Solid(font,"POINTS:",color);
+ apply_surface(POZ-120,4,message,screen);
+ message=make_it_transparent("coin.bmp");
+ apply_surface(POZ+(strlen(nr)*20)-20,0,message,screen);
+}
+
+void print_grid()
+{
+ print_lives();
+ print_points();
+ SDL_Flip(screen);
+}
+
 int main( int argc, char* args[] )
 {
  SDL_Init(SDL_INIT_EVERYTHING);
  screen=SDL_SetVideoMode(MAX_COL,MAX_LIN,32,SDL_HWSURFACE);
+ SDL_WM_SetCaption("Jump!","Pixel RetroGames");
  TTF_Init();
  timy.image_name="warrior.bmp";
- timy.screen_position.x=0;
- timy.screen_position.y=120;
- timy.level_position.x=4;
+ timy.level_position.x=1;
  timy.level_position.y=1;
+ timy.generate_screen_position();
+ timy.horizontal.val=1;
  timy.load_image();
  timy.print_image();
  level TEST;
@@ -167,6 +371,56 @@ int main( int argc, char* args[] )
  TEST.load();
  TEST.print();
  SDL_Flip(screen);
- SDL_Delay(5000);
+ timy.points=123456789;
+ timy.lives=5;
+ print_grid();
+
+ const int FRAMES_PER_SECOND=10;
+ Timer fps;
+ fps.start();
+ int frame=0;
+ SDL_Event event;
+ Uint8 *keystates=SDL_GetKeyState(NULL);
+ bool quit=false;
+ while(!quit)
+       {
+        fps.start();
+        SDL_PollEvent( &event );
+        if(keystates[SDLK_RIGHT])
+           {
+            timy.horizontal.val=1;
+           }
+        if(keystates[SDLK_LEFT])
+           {
+            timy.horizontal.val=-1;
+           }
+        if(keystates[SDLK_SPACE] || keystates[SDLK_UP])
+           {
+            if(TEST.MAP[timy.level_position.x+1][timy.level_position.y].solid)
+               timy.jump();
+           }
+        if(keystates[SDLK_ESCAPE])
+           {
+            quit=true;
+           }
+        if(event.type==SDL_QUIT)
+           quit=true;
+        timy.load_image();
+        update_position(TEST,timy);
+        timy.erase_image();
+        timy.generate_screen_position();
+        timy.print_image();
+        SDL_Flip(screen);
+        //SDL_Delay(500);
+        frame++;
+        if(fps.get_ticks()<1000/FRAMES_PER_SECOND)
+           {
+            SDL_Delay((1000/FRAMES_PER_SECOND)-fps.get_ticks());
+           }
+       }
+ timy.erase_image();
+ timy.generate_screen_position();
+ timy.print_image();
+ SDL_Flip(screen);
 return 0;
 }
